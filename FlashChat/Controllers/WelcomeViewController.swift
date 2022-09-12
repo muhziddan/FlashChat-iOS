2633f76aff49c1566475c5daeb54d4a8124f10b0
muhammad ziddan hidayatullah <ziddan.mz@gmail.com>
2022-09-13 04:32:52 +0700

    Make title label in welcome VC looping

--- FlashChat/Controllers/WelcomeViewController.swift
+++ FlashChat/Controllers/WelcomeViewController.swift
@@ -12,6 +12,15 @@ class WelcomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        titleLabel.text = ""
+        var charIndex = 0
+        let label = "⚡️FlashChat"
+        for char in label {
+            Timer.scheduledTimer(withTimeInterval: 0.1 * Double(charIndex), repeats: false) { timer in
+                self.titleLabel.text?.append(char)
+            }
+            charIndex += 1
+        }
        
     }
     
