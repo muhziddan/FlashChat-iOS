bc24d245dbfc85c281499c751c9f028970bc1d36
muhammad ziddan hidayatullah <ziddan.mz@gmail.com>
2022-09-14 06:57:40 +0700

    Add CLTypingLabel pod and use it to present label in welcome VC

--- FlashChat/Controllers/WelcomeViewController.swift
+++ FlashChat/Controllers/WelcomeViewController.swift
@@ -4,24 +4,16 @@
 //
 
 import UIKit
+import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        titleLabel.text = ""
-        var charIndex = 0
-        let label = "⚡️FlashChat"
-        for char in label {
-            Timer.scheduledTimer(withTimeInterval: 0.1 * Double(charIndex), repeats: false) { timer in
-                self.titleLabel.text?.append(char)
-            }
-            charIndex += 1
-        }
-       
+        
+        titleLabel.text = "⚡️FlashChat"
     }
     
 
---
+++ Podfile
@@ -0,0 +1,10 @@
+platform :ios, '13.0'
+
+target 'FlashChat' do
+  use_frameworks!
+
+  # Pods for FlashChat
+  
+  pod 'CLTypingLabel', '~> 0.4.0'
+
+end
