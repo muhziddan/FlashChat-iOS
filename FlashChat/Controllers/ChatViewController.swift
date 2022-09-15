96f9bd2dea426fa1c840570d6d0bc1409265e03d
muhammad ziddan hidayatullah <ziddan.mz@gmail.com>
2022-09-15 20:26:49 +0700

    Add message model, make dummy data on ChatViewController, and setup tableview with tableview datasource

--- FlashChat/Controllers/ChatViewController.swift
+++ FlashChat/Controllers/ChatViewController.swift
@@ -11,8 +11,16 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
+    var messages: [MessageModel] = [
+        MessageModel(sender: "1@2.com", body: "Hey"),
+        MessageModel(sender: "a@b.com", body: "Hello"),
+        MessageModel(sender: "1@2.com", body: "How is life")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.dataSource = self
 
         title = Constants.applicationName
         navigationItem.hidesBackButton = true
@@ -32,4 +40,28 @@ class ChatViewController: UIViewController {
             print(signOutError)
         }
     }
+    
+    
+}
+
+extension ChatViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.TableView.cellIdentifier, for: indexPath)
+        cell.selectionStyle = .none
+        
+        var cellContent = cell.defaultContentConfiguration()
+        cellContent.text = messages[0].body
+        cellContent.secondaryText = messages[0].sender
+        
+        cell.contentConfiguration = cellContent
+        return cell
+    }
+    
+    
 }
---
+++ FlashChat/Model/MessageModel.swift
@@ -0,0 +1,13 @@
+//
+//  MessageModel.swift
+//  FlashChat
+//
+//  Created by Muhammad Ziddan Hidayatullah on 15/09/22.
+//
+
+import Foundation
+
+struct MessageModel {
+    let sender: String
+    let body: String
+}
