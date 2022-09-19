a4d02ae96a5bbefaff8e12703743c0049d1bb300
muhammad ziddan hidayatullah <ziddan.mz@gmail.com>
2022-09-19 21:02:34 +0700

    Sorted message data using Date() object with .timeIntervalSince1970 method

--- FlashChat/Controllers/ChatViewController.swift
+++ FlashChat/Controllers/ChatViewController.swift
@@ -25,12 +25,14 @@ class ChatViewController: UIViewController {
         navigationItem.hidesBackButton = true
         
         loadMessages()
-        
     }
     
     func loadMessages() {
         
-        db.collection(Constants.FStore.collectionName).addSnapshotListener { querySnapshot, error in
+        db.collection(Constants.FStore.collectionName)
+            .order(by: Constants.FStore.dateField)
+            .addSnapshotListener { querySnapshot, error in
+                
             if let err = error {
                 print(err.localizedDescription)
             } else {
@@ -57,7 +59,8 @@ class ChatViewController: UIViewController {
         
         db.collection(Constants.FStore.collectionName).addDocument(data: [
             Constants.FStore.senderField: messageSender,
-            Constants.FStore.bodyField: messageBody
+            Constants.FStore.bodyField: messageBody,
+            Constants.FStore.dateField: Date().timeIntervalSince1970
         ]) { error in
             if let e = error {
                 print(e.localizedDescription)
