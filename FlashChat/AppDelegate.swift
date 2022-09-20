f02b96a75c9b43b6100115a9803f7a843ae76966
muhammad ziddan hidayatullah <ziddan.mz@gmail.com>
2022-09-20 08:02:28 +0700

    add IQKeyboardManager to manage UI when keyboard apears and set selection style for cell to none

--- FlashChat/AppDelegate.swift
+++ FlashChat/AppDelegate.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 import FirebaseCore
-//import FirebaseFirestore
+import IQKeyboardManagerSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,7 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
-//        let db = Firestore.firestore()
+        IQKeyboardManager.shared.enable = true
+        IQKeyboardManager.shared.enableAutoToolbar = false
+        IQKeyboardManager.shared.shouldResignOnTouchOutside = true
         
         return true
     }
--- Podfile
+++ Podfile
@@ -6,5 +6,6 @@ target 'FlashChat' do
   # Pods for FlashChat
   
   pod 'CLTypingLabel', '~> 0.4.0'
+  pod 'IQKeyboardManagerSwift'
 
 end
